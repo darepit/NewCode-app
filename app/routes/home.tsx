6dcb31b4c76321e41ac7b9b9e86008762a91343b
Mami Bozorgi <32768843+darepit@users.noreply.github.com>
2022-10-31 07:48:01 +0100

    31 OCT

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,15 +2,11 @@ import { json, LoaderFunction } from '@remix-run/node'
 import { getOtherUsers } from '~/utils/user.server'
 import { requireUserId, getUser } from '~/utils/auth.server'
 import { Layout } from '~/components/layout'
-import { UserPanel } from '~/components/user-panel';
 import { useLoaderData, Outlet } from '@remix-run/react';
-
 import { Kudo } from '~/components/kudo';
 import { Kudo as IKudo, Profile, Prisma } from '@prisma/client'
 import { SearchBar } from '~/components/search-bar';
 import { getFilteredKudos, getRecentKudos } from '~/utils/kudos.server'
-import { RecentBar } from '~/components/recent-bar';
-
 interface KudoWithAuthor extends IKudo {
     author: {
         profile: Profile
@@ -78,22 +74,23 @@ export const loader: LoaderFunction = async ({ request }) => {
 };
 
 export default function Home() {
-    const { users, kudos, recentKudos, user } = useLoaderData()
+    const { kudos, user } = useLoaderData()
     return <Layout>
         <Outlet />
-        <div className="h-full flex">
+        <div className="h-full">
             <div className="flex-1 flex flex-col">
                 <SearchBar profile={user.profile} />
-                <div className="flex-1 flex">
-                    <div className="w-full p-10 flex flex-col gap-y-4">
+              
+            </div>
+            <div className="flex-1 flex">
+                    <div className="w-kudo p-10 flex flex-col gap-y-4">
                         {
                             kudos.map((kudo: KudoWithAuthor) =>
-                                <Kudo key={kudo.id} kudo={kudo} profile={kudo.author.profile} />
+                                <Kudo key={kudo.id} kudo={kudo} />
                             )
                         }
                     </div>
                 </div>
-            </div>
         </div>
     </Layout >
 }
--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,4 +1,3 @@
-// login.tsx
 import { useState, useEffect, useRef } from 'react'
 import { Layout } from '~/components/layout'
 import { FormField } from '~/components/form-field'
@@ -113,6 +112,11 @@ export default function Login() {
 
     return (
         <Layout>
+            <br></br>            
+            <br></br>
+            <br></br>
+            <br></br>
+
             <div className="h-full justify-center items-center flex flex-col gap-y-4">
                 {/* Form Switcher Button */}
                 <button
@@ -120,9 +124,9 @@ export default function Login() {
                     className="absolute top-8 right-8 rounded-xl bg-yellow-300 font-semibold text-blue-600 px-3 py-2 transition duration-300 ease-in-out hover:bg-yellow-400 hover:-translate-y-1"
                 >{action === 'login' ? 'Sign Up' : 'Sign In'}</button>
 
-                <h2 className="text-5xl font-extrabold text-yellow-300">Welcome to Kudos!</h2>
+                <h2 className="text-5xl font-extrabold text-yellow-300">Welcome to NewCode!</h2>
                 <p className="font-semibold text-slate-300">{
-                    action === 'login' ? 'Log In To Give Some Praise!' : 'Sign Up To Get Started!'
+                    action === 'login' ? 'Log In To Save Some Codes!' : 'Sign Up To Get Started!'
                 }</p>
                 <form method="POST" className="rounded-2xl bg-gray-200 p-6 w-96">
                     <div className="text-xs font-semibold text-center tracking-wide text-red-500 w-full">
